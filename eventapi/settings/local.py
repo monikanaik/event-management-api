212aa6ea18a243007450c02aa5789304738be095
Monika <naikmonika012@gmail.com>
2024-12-01 17:12:35 +0530

    added files

--- eventapi/settings/local.py
+++ eventapi/settings/local.py
@@ -3,7 +3,9 @@ from datetime import timedelta
 
 from .base import *
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-jg5=-3ej&yie50s#5ga=n9-j5bg78_s*^byys85af1us@_c#3l"
+
 
 DEBUG = True
 
